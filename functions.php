c0e7961e40b6d71e02a4a2796a73f5656a618ab9
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-06-27 17:00:59 +0900

    バリデート追加

--- functions.php
+++ functions.php
@@ -22,3 +22,51 @@ function h($str)
 {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+function insertValidate($company, $name, $email)
+{
+    $errors = [];
+
+    if ($company == '') {
+        $errors[] = MSG_COMPANY_REQUIRED;
+    }
+    if ($name == '') {
+        $errors[] = MSG_NAME_REQUIRED;
+    }
+    if ($email == '') {
+        $errors[] = MSG_EMAIL_REQUIRED;
+    }
+    return $errors;
+}
+function submitCustomersDate($company, $name, $email)
+{
+    try {
+        $dbh = connectDb();
+        $sql = <<<EOM
+        INSERT INTO
+            customers
+            (company, name, email)
+        VALUES
+            (:company, :name, :email);
+        EOM;
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':company', $company, PDO::PARAM_STR);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+function createErrMsg($errors)
+{
+    $err_msg = "<ul class=\"errors\">\n";
+
+    foreach ($errors as $error) {
+        $err_msg .= "<li>" . h($error) . "</li>\n";
+    }
+
+    $err_msg .= "</ul>\n";
+
+    return $err_msg;
+}
--- new.php
+++ new.php
@@ -1,3 +1,29 @@
+<?php
+
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/config.php';
+
+$company = '';
+$name = '';
+$email = '';
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // フォームに入力されたデータを受け取る
+    $company = filter_input(INPUT_POST, 'company');
+    $name = filter_input(INPUT_POST, 'name');
+    $emeil = filter_input(INPUT_POST, 'email');
+    
+    // バリデーション
+    $errors = insertValidate($company, $name, $email);
+    
+    // エラーチェック
+    if (empty($errors)) {
+        // タスク登録処理の実行
+        submitCustomersDate($company, $name, $email);
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -10,7 +36,9 @@
             <h2 class="sub-title">登録</h2>
 
             <ul class="errors">
-                <li></li><!-- エラーメッセージ表示 -->
+                <li>
+                    <?php if ($errors) echo (createErrMsg($errors)) ?>
+                </li>
             </ul>
 
             <form action="" method="post">
@@ -22,7 +50,7 @@
                 <input type="email" id="email" name="email">
                 <input type="submit" class="btn submit-btn" value="追加">
             </form>
-            <a href="" class="btn return-btn">戻る</a>
+            <a href="index.php" class="btn return-btn">戻る</a>
         </div>
     </div>
 </body>
